96aabfd57c86a182138611d5ebbc122dab9ca6f7
IT21170966 <it2117066@my.sliit.lk>
2022-07-19 10:22:28 +0530

    break statement

---
+++ breakAndContinue.php
@@ -0,0 +1,22 @@
+<?php
+    $i=0;
+    for($i = 0; $i <= 5; $i++)
+    {
+        if($i==2)
+        {
+            break;
+        }
+            echo  $i;
+            echo "<br/>";
+    }
+    
+    echo "End of for loop";
+
+
+    
+
+
+
+
+    
+
