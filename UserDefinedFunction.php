2c0b57f15162de3f8071934a47e1fda008893859
IT21170966 <it2117066@my.sliit.lk>
2022-07-19 10:37:39 +0530

    test global function

--- UserDefinedFunction.php
+++ UserDefinedFunction.php
@@ -1,9 +1,25 @@
 <?php
 
-function testFunc()
+/*function testFunc()
 {
     echo "function works well";
 }
 
-testFunc();
+testFunc(); */
+
+//global variable--> It belongs to a paticular page content
+
+$name = "Indu";
+$age = "21";
+
+function testGlobal()
+{
+    global $name, $age;
+
+    $greet = " Hello! to";
+
+    echo $greet . $name . "<br> Age plz!!! <br> " . $age . "<br>";
+}
+
+testGlobal();
 
