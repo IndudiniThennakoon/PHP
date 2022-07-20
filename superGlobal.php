de21189ca00e5a1f38945743117500bbf03d04b2
IT21170966 <it2117066@my.sliit.lk>
2022-07-20 18:46:40 +0530

    Super global php file

---
+++ superGlobal.php
@@ -0,0 +1,16 @@
+<?php
+
+//$_GET
+
+    // echo "Hello ".$_GET["name"]; 
+
+    // In $_GET we can see the value of the name 
+
+//$_POST
+    echo "Age: ". $_POST["age"];
+
+    // but in $_POST method we could nt able to see the value of age
+   
+
+?>
+
